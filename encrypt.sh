5b15d1e7b61e3f70221d1878071430ec390e8540
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-12-10 16:22:45 -0200

    Adds encrypted Travis

--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,1 @@
-tar -czf credentials.tar.gz client-secret.json api_key.js
-sudo travis encrypt-file credentials.tar.gz --add
-git add credentials.tar.gz.enc .travis.yml
-git add --all
-git commit -m "Adds encrypted Travis"
-git push
+sudo tar -czf credentials.tar.gz client-secret.json api_key.js
