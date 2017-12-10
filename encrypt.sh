529aba1449076f268f382f3e97ec75371ab58890
viktorRock <root@DSK_PAULO.localdomain>
2017-12-10 18:14:07 +0000

    Adds encrypted Travis

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,4 @@
-tar -czf credentials.tar.gz client-secret.json api_key.py
+tar -czf credentials.tar.gz client-secret.json api_key.js
 sudo travis encrypt-file credentials.tar.gz --add
 git add credentials.tar.gz.enc .travis.yml
 git add --all
