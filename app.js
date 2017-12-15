a2da93dad2a03e5e0faa53029fd29addf2500589
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-12-15 00:34:53 -0200

    v1.1

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.post('/echo', (req, res) => {
   console.log('body');
   console.log(req.body);
   // res.status(200).json({ message: req.body.message }).end();
-  res.status(SUCESS_STATUS).json({ message: " EchoReply4 - " + req.body.message }).end();
+  res.status(SUCESS_STATUS).json({ message: " EchoReply5 - " + req.body.message }).end();
 });
 
 function authInfoHandler (req, res) {
