221f4b0dfb65db9f7b3e2ebd571fcb550efb93ba
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-12-18 21:14:42 -0200

    pagSeguro

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/echo', (req, res) => {
   console.log('body');
   console.log(req.body);
   // res.status(200).json({ message: req.body.message }).end();
-  res.status(SUCESS_STATUS).json({ message: "Reply - " + req.body.message }).end();
+  res.status(SUCESS_STATUS).json({ message: " Reply - " + req.body.message }).end();
 });
 
 function authInfoHandler (req, res) {
