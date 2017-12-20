64bab6f3255cddfadfda014fb3fd841e9286fbad
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-12-20 21:09:30 -0200

    pagSeguro

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.post('/uolcheckout', (req, res) => {
   });
 });
 app.post('/echo', (req, res) => {
-  console.log('body');
   console.log(req.body);
   // res.status(200).json({ message: req.body.message }).end();
   res.status(SUCESS_STATUS).json({ message: " Reply - " + req.body.message }).end();
