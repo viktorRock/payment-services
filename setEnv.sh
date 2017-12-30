5e0bbaf4f9450a47bf21bc898c54b662af2fbd02
viktorRock <pvmathiassilva@gmail.com>
2017-12-30 21:20:16 -0200

    v2

--- setEnv.sh
+++ setEnv.sh
@@ -1,10 +1,1 @@
-export ENDPOINTS_HOST=https://payment-services.appspot.com
-export ENDPOINTS_KEY=AIzaSyDgvPbx-xkIJ9ymMjFCCGWMVb0oPZAdFC4
-export PAGSEG_CLIENT_EMAIL=pvmathiassilva@gmail.com
-export PAGSEG_CLIENT_TOKEN=E0170E27CBBB495FA1F5FB1F64902066
-export PAGSEG_URL=https://ws.pagseguro.uol.com.br/v2/checkout
-
-sudo apt install ruby ruby-dev
-sudo gem install travis
-sudo travis login --pro
-sudo travis encrypt-file credentials.tar.gz --add
+curl -vv -H 'Content-Type: application/json' -d '@data.json'  "${ENDPOINTS_HOST}/uolcheckout?key=${ENDPOINTS_KEY}"
